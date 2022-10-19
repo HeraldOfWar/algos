634f5d89bb0755bf35fc256837ddb1049b10d729
Jesusya_26 <rusl.babae@yandex.ru>
2022-10-19 16:40:57 +0300

    1

---
+++ lab_2/task_1.py
@@ -0,0 +1,26 @@
+STEP = 0
+
+
+def binary_search(array, element, start, end):
+    """Рекурсивный вариант бинарного поиска"""
+    if start > end:  # если элемент не существует в списке
+        return 'Данного элемента нет в списке!'
+
+    global STEP
+    STEP += 1
+    mid = (start + end) // 2
+    if element == array[mid]:
+        return f'Итоговое количество шагов: {STEP}'
+
+    if element < array[mid]:
+        return binary_search(array, element, start, mid - 1)
+    else:
+        return binary_search(array, element, mid + 1, end)
+
+
+print('Доброго времени суток! Данная программа осуществляет алгоритм бинарного поиска и '
+      'выводит количество шагов для нахождения искомого числа!')
+arr = [int(i) for i in input('Для начала запишите через пробел все целые числа '
+                             'в порядке возрастания, среди которых будет вестись поиск:\n').split()]
+el = int(input('Теперь введите искомое число: '))
+print(binary_search(arr, el, 0, len(arr)))
