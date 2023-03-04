5d1a2d56687d576a12f586633f4ee2aab31afcc3
Jesusya-26 <rus.babaef@yandex.ru>
2023-03-04 21:25:00 +0300

    hash_lab

--- lab_8/task_1.py
+++ lab_8/task_1.py
@@ -6,8 +6,26 @@ def hash_multi(table: dict, key: int, const: float, text: str) -> str:
     n = key * const
     for t in text:
         hash_key = int(len(table.items()) * n)
-        table[hash_key] = t
+        if hash_key not in table.keys():
+            table[hash_key] = []
+        table[hash_key].append(t)
         result += str(hash_key)
+    return result
+
+
+def dehash_multi(table: dict, hash_text: str) -> str:
+    result = ''
+    i, curr_i = -1, 0
+    while i < len(hash_text):
+        i += 1
+        if i == curr_i:
+            if int(hash_text[i]) in table.keys() and table[int(hash_text[i])]:
+                result += table[int(hash_text[i])].pop(0)
+                curr_i = i
+        else:
+            if int(hash_text[curr_i:i]) in table.keys() and table[int(hash_text[curr_i:i])]:
+                result += table[int(hash_text[curr_i:i])].pop(0)
+                curr_i = i
     return result
 
 
@@ -17,6 +35,9 @@ def hash_crc32(s: str) -> str:
 
 s = input('Введите текст, который нужно захэшировать: ')
 hash_table = {}
-k, c = randint(1, 1000), uniform(0, 1)
-print('Хэшированный текст (умножение):', hash_multi(hash_table, k, c, s))
+k, c = randint(1, 100), uniform(0, 1)
+print('Ключ:', str(k), ', константа:', str(c))
+hash_s = hash_multi(hash_table, k, c, s)
+print('Хэшированный текст (умножение):', hash_s)
+print('Исходный текст (умножение):', dehash_multi(hash_table, hash_s))
 print('Хэшированный текст (CRC-32):')
